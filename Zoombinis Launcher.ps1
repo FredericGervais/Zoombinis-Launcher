9e26655ee270176fc976b5cb64fe6806bde59acc
Fred <fred@email.com>
2024-10-11 19:28:04 -0400

    v1.1.0

--- Zoombinis Launcher.ps1
+++ Zoombinis Launcher.ps1
@@ -1,14 +1,19 @@
 
 $StatePath = "$($env:LOCALAPPDATA)\Packages\WD1EncoreSoftwareLLC.Zoombinis_935h8e1rcf9ej\LocalState"
 
+# Find the latest save file
+$LastSaveFile = Get-ChildItem $StatePath\* -Attributes Archive | Where-Object {$_.Length -gt 1024} | Sort-Object -Property LastWriteTime | Select-Object -Last 1
 
-if (Test-Path "$StatePath\playerprefs.dat" -PathType Leaf) {
-    # The Save file is fine
-} else {
-    $LastSaveFile = Get-ChildItem $StatePath\* -Include *.tmp | Select-Object -Last 1
+# If the last save file is not named "playerprefs.dat" rename it so that it is so
+if ("playerprefs.dat" -ne $LastSaveFile.Name){
+    if (Test-Path "$StatePath\playerprefs.dat" -PathType Leaf) {
+        Remove-Item "$StatePath\playerprefs.dat"
+    }
     Rename-Item -Path $LastSaveFile -NewName "playerprefs.dat"
-    Get-ChildItem $StatePath\* -Include *.tmp | Remove-Item
 }
+
+# Remove the old save files
+Get-ChildItem $StatePath\* -Include *.tmp -Attributes Archive | Remove-Item
 
 Start-Process shell:AppsFolder\WD1EncoreSoftwareLLC.Zoombinis_935h8e1rcf9ej!App
 

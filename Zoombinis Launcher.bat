d41853a0bd98b6bee6719fa3b5e8beb61eea07f8
Fred <fred@email.com>
2024-10-13 09:18:58 -0400

    v1.2.0

--- Zoombinis Launcher.bat
+++ Zoombinis Launcher.bat
@@ -4,4 +4,4 @@ SET mypath=%~dp0
 SET launcher=%mypath:~0,-1%\%~n0.ps1
 echo %launcher%
 
-PowerShell -NoProfile -Command "& '%launcher%'"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%launcher%'"
